33941250417a7ed71a299dda4171a1a5b9bc9e82
MetatheGamer <hd524huang@gmail.com>
2024-11-03 06:10:13 -0600

    final-commit

---
+++ scenes/player_character.gd
@@ -0,0 +1,102 @@
+extends CharacterBody2D
+
+@export var all_interactions = []
+@export var cur_interactions = []
+@onready var InteractionLabel = $"InteractionComponents/Interact Label"
+
+var GameManager2 = load("res://scripts/GameManager.gd")
+var utility_instance2 = GameManager
+
+#enum {IDLE, RUN}
+#var state = IDLE
+#@onready var animationTree = $AnimationTree
+#@onready var state_machine = animationTree["parameters/playback"]
+#var blendPosition : Vector2 = Vector2.ZERO
+#var blendPath = [
+	#"parameters/idle/BlendSpace2D/blend_position",
+	#"parameters/move/BlendSpace2D/blend_position"
+#]
+#var anim_tree_keys = [
+	#"idle",
+	#'move'
+#]
+
+
+func _ready():
+	#utility_instance2.Carrots.visible = false
+	update_interactions()
+	
+#Feel free to change the speed
+var SPEED = 500
+var motion = Vector2.ZERO
+
+#Movement of character
+func _physics_process(delta):
+	if Input.is_action_just_pressed("Interact"):
+		exe_interaction()
+	if Input.is_action_pressed("Right") && Input.is_action_pressed("Up"):
+		motion.y = -SPEED
+		motion.x = SPEED
+	elif Input.is_action_pressed("Right") && Input.is_action_pressed("Down"):
+		motion.y = SPEED
+		motion.x = SPEED
+	elif Input.is_action_pressed("Left") && Input.is_action_pressed("Up"):
+		motion.y = -SPEED
+		motion.x = -SPEED
+	elif Input.is_action_pressed("Left") && Input.is_action_pressed("Down"):
+		motion.y = SPEED
+		motion.x = -SPEED
+	elif Input.is_action_pressed("Up"):
+		motion.y = -SPEED
+		motion.x = 0
+	elif Input.is_action_pressed("Down"):
+		motion.y = SPEED
+		motion.x = 0
+	elif Input.is_action_pressed("Left"):
+		motion.x = -SPEED
+		motion.y = 0
+	elif Input.is_action_pressed("Right"):
+		motion.x = SPEED
+		motion.y = 0
+	else:
+		motion.x = 0
+		motion.y = 0
+		
+	velocity = motion
+			
+	move_and_slide()
+
+#func animate() -> void:
+	#state_machine.travel(anim_tree_keys[state])
+	#animationTree.set(blendPath[state], blendPosition)
+################################################################################
+#Interactions
+
+func _on_interaction_area_area_entered(area: Area2D) -> void:
+	all_interactions.insert(0,area)
+	update_interactions()
+
+
+func _on_interaction_area_area_exited(area: Area2D) -> void:
+	all_interactions.erase(area)
+	update_interactions()
+
+func update_interactions():
+	if all_interactions:
+		InteractionLabel.text = all_interactions[0].interact_label
+	#else:
+		#InteractionLabel.text = "" this stopped working. idk why
+		
+var GameManager = load("res://scripts/GameManager.gd")
+var utility_instance = GameManager.new()
+
+		
+func exe_interaction():
+	if all_interactions:
+		var cur_interactions = all_interactions[0]
+		match cur_interactions.interact_type:
+			"print_text" : print(cur_interactions.interact_value)
+			"plant_crops" : utility_instance.choose_crop(cur_interactions.interact_value)
+
+
+#Assorted Variables
--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -22,89 +22,93 @@ var number_of_repeats = 0
 
 var key_pressed = false
 
+@export var all_interactions = []
+@export var cur_interactions = []
+@onready var InteractionLabel = $"InteractionComponents/Interact Label"
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
+	#$Carrots.visible = false
+	#$Potatoes.visible = false
+	#$Soybeans.visible = false
+	#$Wheat.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Night.visible = false
-	choose_crop()
-	harvest()
-	sleep()
+	pass
 	
 	#print("harvest_time: ", harvest_time)
 	#print("Balance: ", balance)
-	
-func choose_crop():
-	if Input.is_action_pressed("Carrots") && harvest_time == 0:
+#Plants crops
+
+
+func choose_crop(type):
+	if type == "Carrots" && harvest_time == 0:
 		current_crops = "Carrots"
 		harvest_time = 2
-		$Carrots.visible = true
+		print("was meant to plant carrots")
 		#Something to make it plant carrots
-	elif Input.is_action_pressed("Potatoes") && harvest_time == 0:
+	elif type == "Potatoes" && harvest_time == 0:
 		current_crops = "Potatoes"
 		harvest_time = 2
 		$Potatoes.visible = true
 		#Something to make it plant Potatoes
-	elif Input.is_action_pressed("Wheat") && harvest_time == 0:
+	elif type == "Wheat" && harvest_time == 0:
 		current_crops = "Wheat"
 		harvest_time = 5
 		$Wheat.visible = true
 		#Something to make it plant WHeat
-	elif Input.is_action_pressed("Soybeans") && harvest_time == 0:
+	elif type == "Soybeans" && harvest_time == 0:
 		current_crops = "Soybeans"
 		harvest_time = 4
 		$Soybeans.visible = true
 		#Something to make it plant Soybeans
 
+	
+var type
+	
+
+
 func loss():
-	if previous_crop == current_crops && harvest_time == 0:
-		
-		yeild_lost 
+	pass
 	
 	
 
-func harvest():
-	if harvest_time == 0:
-		if current_crops == "Carrots":
-			balance += 460130
-		elif current_crops == "Potatoes":
-			balance += 890000
-		elif current_crops == "Wheat":
-			balance += 66750
-		elif current_crops == "Soybeans":
-			balance += 157530
+#func harvest():
+	#if harvest_time == 0:
+		#if current_crops == "Carrots":
+		#	balance += 460130
+	#elif current_crops == "Potatoes":
+		#	balance += 890000
+	#elif current_crops == "Wheat":
+		#	balance += 66750
+	#elif current_crops == "Soybeans":
+		#	balance += 157530
 	
 	
 			
-	$Carrots.visible = false
-	$Potatoes.visible = false
-	$Wheat.visible = false
-	$Soybeans.visible = false
+	#$Carrots.visible = false
+	#$Potatoes.visible = false
+	#$Wheat.visible = false
+	#$Soybeans.visible = false
 	
-	previous_crop = current_crops
+	#previous_crop = current_crops
 
 func wait(seconds: float):
 	await get_tree().create_timer(seconds).timeout
 	
 func sleep():
 	var night_time = false
-	if Input.is_action_pressed("Sleep") && key_pressed == false:
-		night_time = true
-		months += 1
-		harvest_time -= 1
-		key_pressed = true
-		
-		var control = 10099999000
-		
-		while control > 0:
-			control -= 1
-			$Night.visible = true
+	#if ("Sleep") && key_pressed == false:
+		#night_time = true
+		#months += 1
+		#harvest_time -= 1
+		#key_pressed = true
 		
 		
-	key_pressed = false
+		
+	#key_pressed = false
 		
 		
 	
